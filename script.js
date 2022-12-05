1de52c4ece96f84a810229316aa118faaa5e0120
SantiNGSA <snunez2900@esdmadrid.es>
2022-12-05 17:47:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,343 @@
+window.onload = function() {
+
+    let titleLib = this.document.querySelector("#icono_lib");
+    
+    let h1weightLib = this.document.querySelector("#h1weight_lib");
+    let h1animLib = this.document.querySelector("#h1anim_lib");
+
+    h1weightLib.addEventListener('input', function () {
+        titleLib.style.setProperty('--weightH1', h1weightLib.value);        
+    }, false);
+    h1animLib.addEventListener('input', function () {
+        titleLib.style.setProperty('--animH1', h1animLib.value);        
+    }, false);
+
+    let titlePana = this.document.querySelector("#icono_pana");
+    
+    let h1weightPana = this.document.querySelector("#h1weight_pana");
+    let h1animPana = this.document.querySelector("#h1anim_pana");
+
+    h1weightPana.addEventListener('input', function () {
+        titlePana.style.setProperty('--weightH1', h1weightPana.value);        
+    }, false);
+    h1animPana.addEventListener('input', function () {
+        titlePana.style.setProperty('--animH1', h1animPana.value);        
+    }, false);
+    
+    // JOYERIA
+
+    let titleJoye = this.document.querySelector("#icono_joye");
+    
+    let h1weightJoye = this.document.querySelector("#h1weight_joye");
+    let h1animJoye = this.document.querySelector("#h1anim_joye");
+
+    h1weightJoye.addEventListener('input', function () {
+        titleJoye.style.setProperty('--weightH1', h1weightJoye.value);        
+    }, false);
+    h1animJoye.addEventListener('input', function () {
+        titleJoye.style.setProperty('--animH1', h1animJoye.value);        
+    }, false);
+
+
+    // PESCADERÍA
+
+    let titlePesc = this.document.querySelector("#icono_pesc");
+    
+    let h1weightPesc = this.document.querySelector("#h1weight_pesc");
+    let h1animPesc = this.document.querySelector("#h1anim_pesc");
+
+    h1weightPesc.addEventListener('input', function () {
+        titlePesc.style.setProperty('--weightH1', h1weightPesc.value);        
+    }, false);
+    h1animPesc.addEventListener('input', function () {
+        titlePesc.style.setProperty('--animH1', h1animPesc.value);        
+    }, false);
+
+
+    // RESTAURANTE
+    let titleRest = this.document.querySelector("#icono_rest");
+    
+    let h1weightRest = this.document.querySelector("#h1weight_rest");
+    let h1animRest = this.document.querySelector("#h1anim_rest");
+
+    h1weightRest.addEventListener('input', function () {
+        titleRest.style.setProperty('--weightH1', h1weightRest.value);        
+    }, false);
+    h1animRest.addEventListener('input', function () {
+        titleRest.style.setProperty('--animH1', h1animRest.value);        
+    }, false);
+
+
+     // COMIDA RAPIDA
+     let titleCrap = this.document.querySelector("#icono_crapida");
+    
+     let h1weightCrap = this.document.querySelector("#h1weight_crap");
+     let h1animCrap = this.document.querySelector("#h1anim_crap");
+ 
+     h1weightCrap.addEventListener('input', function () {
+         titleCrap.style.setProperty('--weightH1', h1weightCrap.value);        
+     }, false);
+     h1animCrap.addEventListener('input', function () {
+         titleCrap.style.setProperty('--animH1', h1animCrap.value);        
+     }, false);
+
+     // JUEGOS MESA
+     let titleJmesa = this.document.querySelector("#icono_jmesa");
+    
+     let h1weightJmesa = this.document.querySelector("#h1weight_jmesa");
+     let h1animJmesa = this.document.querySelector("#h1anim_jmesa");
+ 
+     h1weightJmesa.addEventListener('input', function () {
+         titleJmesa.style.setProperty('--weightH1', h1weightJmesa.value);        
+     }, false);
+     h1animJmesa.addEventListener('input', function () {
+         titleJmesa.style.setProperty('--animH1', h1animJmesa.value);        
+     }, false);
+
+           // ROPA
+           let titleRopa = this.document.querySelector("#icono_ropa");
+    
+           let h1weightRopa = this.document.querySelector("#h1weight_ropa");
+           let h1animRopa = this.document.querySelector("#h1anim_ropa");
+       
+           h1weightRopa.addEventListener('input', function () {
+               titleRopa.style.setProperty('--weightH1', h1weightRopa.value);        
+           }, false);
+           h1animRopa.addEventListener('input', function () {
+               titleRopa.style.setProperty('--animH1', h1animRopa.value);        
+           }, false);
+     
+
+  // OPTICA
+  let titleOpt = this.document.querySelector("#icono_opt");
+    
+  let h1weightOpt = this.document.querySelector("#h1weight_opt");
+  let h1animOpt = this.document.querySelector("#h1anim_opt");
+
+  h1weightOpt.addEventListener('input', function () {
+      titleOpt.style.setProperty('--weightH1', h1weightOpt.value);        
+  }, false);
+  h1animOpt.addEventListener('input', function () {
+      titleOpt.style.setProperty('--animH1', h1animOpt.value);        
+  }, false);
+
+      // PERFUMERIA
+      let titlePerf = this.document.querySelector("#icono_perf");
+    
+      let h1weightPerf = this.document.querySelector("#h1weight_perf");
+      let h1animPerf = this.document.querySelector("#h1anim_perf");
+  
+      h1weightPerf.addEventListener('input', function () {
+          titlePerf.style.setProperty('--weightH1', h1weightPerf.value);        
+      }, false);
+      h1animPerf.addEventListener('input', function () {
+          titlePerf.style.setProperty('--animH1', h1animPerf.value);        
+      }, false);
+
+    // ANIMALES
+    let titleAnima = this.document.querySelector("#icono_anima");
+        
+    let h1weightAnima = this.document.querySelector("#h1weight_anima");
+    let h1animAnima = this.document.querySelector("#h1anim_anima");
+
+    h1weightAnima.addEventListener('input', function () {
+        titleAnima.style.setProperty('--weightH1', h1weightAnima.value);        
+    }, false);
+    h1animAnima.addEventListener('input', function () {
+        titleAnima.style.setProperty('--animH1', h1animAnima.value);        
+    }, false);
+
+
+      // PINTURA
+      let titlePint = this.document.querySelector("#icono_pint");
+    
+      let h1weightPint = this.document.querySelector("#h1weight_pint");
+      let h1animPint = this.document.querySelector("#h1anim_pint");
+  
+      h1weightPint.addEventListener('input', function () {
+          titlePint.style.setProperty('--weightH1', h1weightPint.value);        
+      }, false);
+      h1animPint.addEventListener('input', function () {
+          titlePint.style.setProperty('--animH1', h1animPint.value);        
+      }, false);
+
+
+ // CERRAJERIA
+ let titleCerra = this.document.querySelector("#icono_cerra");
+    
+ let h1weigthCerra = this.document.querySelector("#h1weight_cerra");
+ let h1animCerra = this.document.querySelector("#h1anim_cerra");
+
+ h1weigthCerra.addEventListener('input', function () {
+     titleCerra.style.setProperty('--weightH1', h1weigthCerra.value);        
+ }, false);
+ h1animCerra.addEventListener('input', function () {
+     titleCerra.style.setProperty('--animH1', h1animCerra.value);        
+ }, false);
+
+           // FRUTERIA
+           let titleFrut = this.document.querySelector("#icono_frut");
+    
+           let h1weigthFrut = this.document.querySelector("#h1weight_frut");
+           let h1animFrut = this.document.querySelector("#h1anim_frut");
+       
+           h1weigthFrut.addEventListener('input', function () {
+               titleFrut.style.setProperty('--weightH1', h1weigthFrut.value);        
+           }, false);
+           h1animFrut.addEventListener('input', function () {
+               titleFrut.style.setProperty('--animH1', h1animFrut.value);        
+           }, false);
+
+      // ELECTRONICA
+      let titleElec = this.document.querySelector("#icono_elec");
+    
+      let h1weigthElec = this.document.querySelector("#h1weight_elec");
+      let h1animElec = this.document.querySelector("#h1anim_elec");
+  
+      h1weigthElec.addEventListener('input', function () {
+          titleElec.style.setProperty('--weightH1', h1weigthElec.value);        
+      }, false);
+      h1animElec.addEventListener('input', function () {
+          titleElec.style.setProperty('--animH1', h1animElec.value);        
+      }, false);
+
+      // ISOTIPO LOCVI
+      let titleIso = this.document.querySelector("#icono_iso");
+    
+      let h1weigthIso = this.document.querySelector("#h1weight_iso");
+      let h1animIso = this.document.querySelector("#h1anim_iso");
+  
+      h1weigthIso.addEventListener('input', function () {
+          titleIso.style.setProperty('--weightH1', h1weigthIso.value);        
+      }, false);
+      h1animIso.addEventListener('input', function () {
+          titleIso.style.setProperty('--animH1', h1animIso.value);        
+      }, false);
+
+       // BAR
+       let titleBar = this.document.querySelector("#icono_bar");
+    
+       let h1weigthBar = this.document.querySelector("#h1weight_bar");
+       let h1animBar = this.document.querySelector("#h1anim_bar");
+   
+       h1weigthBar.addEventListener('input', function () {
+           titleBar.style.setProperty('--weightH1', h1weigthBar.value);        
+       }, false);
+       h1animBar.addEventListener('input', function () {
+           titleBar.style.setProperty('--animH1', h1animBar.value);        
+       }, false);
+
+       // FOTOGRAFIA
+       let titleFoto = this.document.querySelector("#icono_foto");
+    
+       let h1weigthFoto = this.document.querySelector("#h1weight_foto");
+       let h1animFoto = this.document.querySelector("#h1anim_foto");
+   
+       h1weigthFoto.addEventListener('input', function () {
+           titleFoto.style.setProperty('--weightH1', h1weigthFoto.value);        
+       }, false);
+       h1animFoto.addEventListener('input', function () {
+           titleFoto.style.setProperty('--animH1', h1animFoto.value);        
+       }, false);
+
+
+        // PELUQUERIA
+        let titlePlqra = this.document.querySelector("#icono_plqra");
+    
+        let h1weigthPlqra = this.document.querySelector("#h1weight_plqra");
+        let h1animPlqra = this.document.querySelector("#h1anim_plqra");
+    
+        h1weigthPlqra.addEventListener('input', function () {
+            titlePlqra.style.setProperty('--weightH1', h1weigthPlqra.value);        
+        }, false);
+        h1animPlqra.addEventListener('input', function () {
+            titlePlqra.style.setProperty('--animH1', h1animPlqra.value);        
+        }, false);
+
+        // CAFETERIA
+        let titleCftra = this.document.querySelector("#icono_cftra");
+    
+        let h1weigthCftra = this.document.querySelector("#h1weight_cftra");
+        let h1animCftra = this.document.querySelector("#h1anim_cftra");
+    
+        h1weigthCftra.addEventListener('input', function () {
+            titleCftra.style.setProperty('--weightH1', h1weigthCftra.value);        
+        }, false);
+        h1animCftra.addEventListener('input', function () {
+            titleCftra.style.setProperty('--animH1', h1animCftra.value);        
+        }, false);
+
+
+        let radarWord = this.document.querySelector("#radar1");
+
+        let sliderRadar = this.document.querySelector("#sliderRadar");
+        
+
+        sliderRadar.addEventListener('input', function (){
+            radarWord.style.setProperty('--weightH2', sliderRadar.value);
+        }, false);
+
+
+
+
+        let radarIcon = this.document.querySelector("#radar1_icon");
+
+        let slider2Radar = this.document.querySelector("#sliderRadar");
+        
+
+        slider2Radar.addEventListener('input', function (){
+            radarIcon.style.setProperty('--radar2', sliderRadar.value);
+        }, false);
+
+
+       
+ 
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var outputWght = document.getElementById("demoWght");
+
+// var sliderWght = document.getElementById("myRangeWght").oninput = function(){
+//     var value = (this.value-this.min)/(this.max-this.min)*100
+
+    
+
+//     outputWght.innerHTML =this.value
+
+//     document.querySelector('.icono').getElementsByClassName.setProperty('font-variation-settings',`$(sliderWght.value)wght`)
+
+// }
+
+
+
+
+
+
+// var outputAnim = document.getElementById("demoAnim");
+
+// var sliderAnim = document.getElementById("myRangeAnim").oninput = function(){
+//     var value = (this.value-this.min)/(this.max-this.min)*100
+
+    
+
+//     outputAnim.innerHTML =this.value
+// }
